d1349fb0522d40dbc491684272c136eddc824aa6
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2015-01-15 13:49:57 -0800

    Initial Commit

---
+++ DelegateExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegateExample
+//
+//  Created by Erick Bennett on 1/15/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
